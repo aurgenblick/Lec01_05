a87fd00e0ba2eb27e651e0ea05d1b8e209ef250a
aurgenblick <aurgenblick@gmail.com>
2022-09-08 10:31:12 +0300

    initial

---
+++ src/WhiteBoxTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhiteBoxTest {
+
+	@Before
+	public void setUp() throws Exception {
+		wb = new WhiteBox();
+	}
+
+
+	private WhiteBox wb;
+}
